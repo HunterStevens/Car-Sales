6f59b3b3b47994ef0da3150f6372c0c8dc124c5d
Hunter Stevens <hunterstevens1996@gmail.com>
2020-04-14 14:53:15 -0600

    created CarActions with add/remove features function

--- src/actions/CarActions.js
+++ src/actions/CarActions.js
@@ -1,1 +1,9 @@
-export const
+export const addFeatures = () =>{
+    console.log("addFeature function running");
+
+    return{type:'ADD_FEATURES'};
+}
+
+export const removeFeatures = feature =>{
+    return{type:'REMOVE_FEATURES', payload:feature};
+}
