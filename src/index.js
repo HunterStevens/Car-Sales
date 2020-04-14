b0c4a9ffa8b0e325a18bb017108fa84d6ab858db
Hunter Stevens <hunterstevens1996@gmail.com>
2020-04-14 13:25:54 -0600

    initail set up with Redux installed, Created FeaturesReducer, attemtping to import it in index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import FeaturesReducer from './reducers/FeaturesReducer';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(FeaturesReducer);
+console.log("CreateStore - store:", store);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider>
+<App />
+</Provider>    , rootElement);
